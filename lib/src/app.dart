00c49efc3222264227cafe95e8deede2799f1989
zacharias <zacharias@polonius.dk>
2023-11-07 20:31:03 +0100

    added dummy screen

---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  Widget build(context){
+return MaterialApp(
+title: 'Log Me In Please!',
+home: Scaffold(
+  body: Text('Please make a login form here!'),
+),
+);
+  }
+}
