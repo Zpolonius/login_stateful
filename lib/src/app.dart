a1da5c401ece07b4f5dc075923c240ca16663081
zacharias <zacharias@polonius.dk>
2023-11-07 20:38:34 +0100

    added const and override

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
+  @override
   Widget build(context){
-return MaterialApp(
+return const  MaterialApp(
 title: 'Log Me In Please!',
 home: Scaffold(
-  body: Text('Please make a login form here!'),
+  body: Text('Please add a login form here!'),
 ),
 );
   }
