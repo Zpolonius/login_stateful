421efdabf9c2e309c053974e7464a5f0169962d5
zacharias <zacharias@polonius.dk>
2023-11-08 19:47:46 +0100

    made login_screen file and folder

---
+++ lib/src/Screens/login_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget{
+  const LoginScreen({super.key});
+
+  
+  
+   
+     @override
+  createState() {
+    
+    return LoginScreenState();
+  }
+ 
+}
+ class LoginScreenState extends State<LoginScreen>{
+    
+      @override
+  Widget build(){
+
+    }
+  }
